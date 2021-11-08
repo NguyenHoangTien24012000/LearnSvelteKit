82ebbf0e79ec10c10b542077cfaecbc52c4b5221
NguyenHoangTien24012000 <hoangtienflytom@gmail.com>
2021-11-08 17:59:22 +0700

    Step 1

---
+++ my-app/src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Nav from "$lib/Nav.svelte";
+</script>
+
+
+<div class="container">
+
+</div>
+<Nav/>
+  <slot></slot>
---
+++ my-app/src/routes/blog/firstpost.svelte
@@ -0,0 +1,2 @@
+<h2>Fist Post</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quo minima dolores voluptates harum velit odio, repellendus sapiente quisquam veniam.</p>
